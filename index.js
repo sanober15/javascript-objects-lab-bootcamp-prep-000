0b095497b773e4f3f945e31d7a82cf1c988d2edf
sanober15 <sanober@sanober.me>
2017-12-04 10:35:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object, {[key]:value});
 }
+
+function deleteFromObjectByKey(object, key){
+  var x = Object.assign({}, object, {[key]:value});
+  x;
+  delete x.key;
+  x;
+  return x;
+}
